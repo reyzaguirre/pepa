255875c303059a57c13ca6a0efe24ca5d9f8ce88
reyzaguirre <reyzaguirre@gmail.com>
2015-10-26 15:31:32 -0500

    met repo ready

--- R/repo_met.R
+++ R/repo_met.R
@@ -1,8 +1,8 @@
 #' Authomatic report for a MET with a RCBD
 #'
-#' Produces an authomatic report for a set of selected traits in a multi environment
+#' Produces an authomatic report for a selected trait in a multi environment
 #' trial (MET) with a RCBD in each environment.
-#' @param traits The list of traits to include.
+#' @param trait The trait to be evaluated.
 #' @param geno The genotypes.
 #' @param env The environments.
 #' @param rep The replications.
@@ -10,23 +10,23 @@
 #' @param maxp Maximum allowed proportion of missing values to estimate, default is 10\%.
 #' @param author Author.
 #' @author Raul Eyzaguirre.
-#' @details It fits a linear model for a MET with a RCBD for each trait.
+#' @details It fits a linear model for a MET with a RCBD for the selected trait.
 #' If data is unbalanced, missing values are estimated up to an specified maximum
 #' proportion, 10\% by default. Genotypes and environments are considered as fixed
 #' factors while the blocks are considered as random and nested into the environments.
 #' @return It returns an automatic report about the MET with a RCBD fitted model.
 #' @examples
-#' repo.met(c("rytha", "bc", "dm"), "geno", "loc", "rep", spg)
+#' repo.met("rytha", "geno", "env", "rep", megaclones)
 #' @export
 
-repo.met <- function(traits, geno, env, rep, data, maxp = 0.1,
+repo.met <- function(trait, geno, env, rep, data, maxp = 0.1,
                      author = "International Potato Center") {
 
   dirfiles <- system.file(package = "pepa")
   fileRmd <- paste(dirfiles, "/met.Rmd", sep = "")
   fileURL <- paste(dirfiles, "/met.html", sep = "")
 
-  rmarkdown::render(fileRmd, params = list(traits = traits,
+  rmarkdown::render(fileRmd, params = list(trait = trait,
                                            geno = geno,
                                            env = env,
                                            rep = rep,
