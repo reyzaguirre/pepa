186f35094c7727e34fe73bc031be028d5adfe7ba
reyzaguirre <reyzaguirre@gmail.com>
2016-04-15 10:32:27 -0500

    repo_rcbd full

--- R/repo_rcbd.R
+++ R/repo_rcbd.R
@@ -9,10 +9,21 @@
 #' @param author Author.
 #' @author Raul Eyzaguirre.
 #' @details It fits a linear model for a RCBD and explains the results.
-#' It also checks the assumptions.
+#'
+#' Under the assumption of fixed effects an ANOVA table is computed with missing values
+#' estimated up to a specified percentage (10\% by default). If the ANOVA results in
+#' a significant value for treatments then the Tukey HSD method for pairwise difference
+#' is applied. Assumptions of the model are illustrated with residual plots.
+#'
+#' Under the assumption of random effects the model is estimated using REML and the
+#' variance components are shown. Missing values are not estimated in this case.
 #' @return It returns an explanation about the RCBD fitted model.
 #' @examples
 #' repo.rcbd(c("trw", "vw", "crw"), "geno", "rep", pjpz09)
+#'
+#' # With a small data set
+#' temp <- pjpz09[1:18, ]
+#' repo.rcbd(c("trw", "vw", "crw"), "geno", "rep", temp)
 #' @export
 
 repo.rcbd <- function(traits, treat, rep, data, maxp = 0.1,
