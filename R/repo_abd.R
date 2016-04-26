defc91178c515c23f069440aeb657d36523d3531
reyzaguirre <reyzaguirre@gmail.com>
2016-04-26 15:00:39 -0500

    new function repo for abd under development

---
+++ R/repo_abd.R
@@ -0,0 +1,34 @@
+#' Authomatic report for an augmented block design (ABD)
+#'
+#' Produces an authomatic report for selected traits in an experiment with an ABD.
+#' @param traits The traits to analize.
+#' @param treat The treatments.
+#' @param rep The replications.
+#' @param data The name of the data frame.
+#' @param author Author.
+#' @author Raul Eyzaguirre.
+#' @details It fits a linear model for an ABD and explains the results.
+#'
+#' The Tukey HSD method for pairwise differences is applied.
+#' Assumptions of the model are evaluated with residual plots.
+#'
+#' @return It returns an explanation about the ABD fitted model.
+#' @examples
+#' # A subset that looks like an ABD
+#' temp <- pjpz09[c(1, 2, 9, 10, 13, 14, 27, 29, 31, 33, 35, 37, 40, 42, 44, 46, 48, 50), ]
+#' repo.abd(c("trw", "vw", "crw"), "geno", "rep", temp)
+#' @export
+
+repo.abd <- function(traits, treat, rep, data, author = "International Potato Center") {
+
+  dirfiles <- system.file(package = "pepa")
+  fileRmd <- paste(dirfiles, "/rmd/abd.Rmd", sep = "")
+  fileURL <- paste(dirfiles, "/rmd/abd.html", sep = "")
+
+  rmarkdown::render(fileRmd, params = list(traits = traits,
+                                           treat = treat,
+                                           rep = rep,
+                                           data = data,
+                                           author = author))
+  browseURL(fileURL)
+}
