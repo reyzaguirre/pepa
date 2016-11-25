b6f42cbe4d7b56d95935b5525cfb93fbaaec0b14
reyzaguirre <reyzaguirre@gmail.com>
2016-11-25 14:19:39 -0500

    data sets por pvs

---
+++ R/data_PVS.R
@@ -0,0 +1,69 @@
+#' @name pvsf1
+#' @title Voting for selection criteria
+#' @docType data
+#' @aliases pvsf1
+#' @description This data set has the voting of men (\code{SCORE_MEN}),
+#' women (\code{SCORE_WOMEN}), and global voting (\code{SCORE_GLOBAL}) for
+#' the more importan selection criteria (\code{IDENTIFIED_CRITERIA}) on
+#' different phases (\code{PHASE}).
+#' @usage pvsf1
+#' @format A data frame with 5 columns and 15 rows.
+#' @source International Potato Center, potato data.
+NULL
+#' @name pvsf2
+#' @title Voting for best clones at flowering
+#' @docType data
+#' @aliases pvsf2
+#' @description This data set has the voting of men (\code{MSM}), women (\code{MSWM}),
+#' and global voting (\code{MSGLO}) in the mother plot, and the voting of men
+#' (\code{BSM}), women (\code{BSWM}), and global voting (\code{BSGLO}) in the baby
+#' plots for the best clones (\code{INSTN}) at flowering.
+#' @usage pvsf2
+#' @format A data frame with 8 columns and 18 rows.
+#' @source International Potato Center, potato data.
+NULL
+#' @name pvsf3
+#' @title Voting for best clones at harvest
+#' @docType data
+#' @aliases pvsf3
+#' @description This data set has the voting of men (\code{MSM}), women (\code{MSWM}),
+#' and global voting (\code{MSGLO}) in the mother plot, and the voting of men
+#' (\code{BSM}), women (\code{BSWM}), and global voting (\code{BSGLO}) in the baby
+#' plots for the best clones (\code{INSTN}) at harvest.
+#' @usage pvsf3
+#' @format A data frame with 8 columns and 18 rows.
+#' @source International Potato Center, potato data.
+NULL
+#' @name pvsf6
+#' @title Organoleptic scores at mother trial
+#' @docType data
+#' @aliases pvsf6
+#' @description This data set has the scores given by men and women to several clones
+#' (\code{INSTN}), for attributes \code{APPEARANCE}, \code{TASTE}, and \code{TEXTURE}
+#' at the mother trial.
+#' @usage pvsf6
+#' @format A data frame with 6 columns and 50 rows.
+#' @source International Potato Center, potato data.
+NULL
+#' @name pvsf7
+#' @title Organoleptic scores at baby trials
+#' @docType data
+#' @aliases pvsf7
+#' @description This data set has the scores given by men and women to several clones
+#' (\code{INSTN}), for attributes \code{APPEARANCE}, \code{TASTE}, and \code{TEXTURE}
+#' at the baby trials.
+#' @usage pvsf7
+#' @format A data frame with 6 columns and 50 rows.
+#' @source International Potato Center, potato data.
+NULL
+#' @name pvsf9
+#' @title Voting for best clones at post-harvest
+#' @docType data
+#' @aliases pvsf9
+#' @description This data set has the voting of men (\code{SCORE_MEN}), women
+#' (\code{SCORE_WOMEN}), and global voting (\code{SCORE_GLOBAL}) for the best
+#' clones (\code{INSTN}) at post-harvest with different storage types (\code{STYPE}).
+#' @usage pvsf9
+#' @format A data frame with 6 columns and 18 rows.
+#' @source International Potato Center, potato data.
+NULL
