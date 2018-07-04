cf75fdfc7d8d5c0e504f8a09f4a4022b8a887532
reyzaguirre <reyzaguirre@gmail.com>
2018-07-04 13:59:32 -0500

    repo.2f => repo.f

--- R/repo_f.R
+++ R/repo_f.R
@@ -1,10 +1,9 @@
-#' Automatic report for a 2-factor factorial
+#' Automatic report for a factorial experiment
 #'
-#' Produces an automatic report for selected traits in a 2-factor factorial with
+#' Produces an automatic report for selected traits in a factorial experiment with
 #' a CRD or RCBD.
 #' @param traits The traits to analize.
-#' @param A Factor A.
-#' @param B Factor B.
+#' @param factors The factors (up to 3).
 #' @param rep The replications or blocks.
 #' @param design The statistical design, \code{crd} or \code{rcbd}.
 #' @param data The name of the data frame containing the data.
@@ -15,22 +14,22 @@
 #' @param format The output file format for the report, \code{"html"} by default.
 #' Other options are \code{"word"} and \code{"pdf"}.
 #' @author Raul Eyzaguirre.
-#' @details It fits a linear model for a 2-factor factorial with a CRD or RCBD for
-#' the selected trait. If data is unbalanced, missing values are estimated up to an
-#' specified maximum proportion, 10\% by default. Both factors are considered as fixed.
-#' @return It returns an automatic report about the 2-factor factorial with a CRD or
+#' @details It fits a linear model for a factorial experiment with a CRD or RCBD for
+#' the selected traits. If data is unbalanced, missing values are estimated up to an
+#' specified maximum proportion, 10\% by default. All factors are considered as fixed.
+#' @return It returns an automatic report for the factorial experiment with a CRD or
 #' RCBD fitted model.
 #' @examples
-#' repo.2f(c("asc.dw", "asc.fw"), "geno", "treat", "rep", "crd", asc)
+#' repo.f(c("asc.dw", "asc.fw"), c("geno", "treat"), "rep", "crd", asc)
 #' @import agricolae
 #' @importFrom utils browseURL
 #' @export
 
-repo.2f <- function(traits, A, B, rep, design, data, maxp = 0.1,
-                     title = "Automatic report for a 2-factor factorial",
-                     subtitle = NULL,
-                     author = "International Potato Center",
-                     format = c("html", "word", "pdf")) {
+repo.f <- function(traits, factors, rep, design, data, maxp = 0.1,
+                   title = "Automatic report for a factorial experiment",
+                   subtitle = NULL,
+                   author = "International Potato Center",
+                   format = c("html", "word", "pdf")) {
 
   format <- paste(match.arg(format), "_document", sep = "")
   dirfiles <- system.file(package = "pepa")
@@ -50,8 +49,7 @@ repo.2f <- function(traits, A, B, rep, design, data, maxp = 0.1,
 
   rmarkdown::render(fileRmd, output_format = format,
                     params = list(traits = traits,
-                                  A = A,
-                                  B = B,
+                                  factors = factors,
                                   rep = rep,
                                   design = design,
                                   data = data,
