3df6c2da53db6fe2f1aace8ad674eb9b67e24725
reyzaguirre <reyzaguirre@gmail.com>
2018-08-06 09:50:13 -0500

    updated

--- R/repo_a01d.R
+++ R/repo_a01d.R
@@ -1,4 +1,4 @@
-#' Automatic report for an alpha (0,1) design.
+#' Automatic report for an alpha (0,1) design
 #'
 #' Produces an automatic report for selected traits in an experiment with an
 #' alpha (0,1) design.
@@ -7,28 +7,28 @@
 #' @param rep The replications.
 #' @param block The blocks.
 #' @param k The size of the blocks.
-#' @param data The name of the data frame.
-#' @param title The title.
-#' @param subtitle The subtitle.
-#' @param author Author.
+#' @param dfr The name of the data frame.
+#' @param title Report title.
+#' @param subtitle Report subtitle.
+#' @param author Report author.
 #' @param format The output file format for the report, \code{"html"} by default.
 #' Other options are \code{"word"} and \code{"pdf"}.
 #' @param server Logical. If \code{"FALSE"} works with local machines.
 #' Otherwise works in server environments.
 #' @param server_dir_name If \code{"server = TRUE"}, this is the directory name in the server.
 #' @param server_file_name If \code{"server = TRUE"}, this is the file name in the server.
-#' @author Raul Eyzaguirre.
 #' @details It fits a linear model for an alpha (0,1) design and explains the results.
 #' An alpha (0,1) design is an incomplete block design that is resolvable.
 #' In a resolvable design the incomplete blocks group together in complete replications.
 #' @return It returns an explanation about the alpha (0,1) design fitted model.
+#' @author Raul Eyzaguirre.
 #' @examples
 #' repo.a01d("yield", "geno", "rep", "block", k = 3, a01data)
 #' @import st4gi
 #' @importFrom utils browseURL
 #' @export
 
-repo.a01d <- function(traits, geno, rep, block, k, data,
+repo.a01d <- function(traits, geno, rep, block, k, dfr,
                       title = "Automatic report for an alpha (0,1) design",
                       subtitle = NULL,
                       author = "International Potato Center",
@@ -66,7 +66,7 @@ repo.a01d <- function(traits, geno, rep, block, k, data,
                                   rep = rep,
                                   block = block,
                                   k = k,
-                                  data = data,
+                                  dfr = dfr,
                                   title = title,
                                   subtitle = subtitle,
                                   author = author))
