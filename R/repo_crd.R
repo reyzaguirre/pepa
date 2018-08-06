4d9bac97f07b146c9396f46addef43aa324e3f1f
reyzaguirre <reyzaguirre@gmail.com>
2018-08-06 16:30:34 -0500

    updated

--- R/repo_crd.R
+++ R/repo_crd.R
@@ -3,18 +3,17 @@
 #' Produces an automatic report for selected traits in an experiment with a CRD.
 #' @param traits The traits to analize.
 #' @param geno The genotypes.
-#' @param data The name of the data frame.
+#' @param dfr The name of the data frame.
 #' @param maxp Maximum allowed proportion of missing values to estimate, default is 10\%.
-#' @param title The title.
-#' @param subtitle The subtitle.
-#' @param author Author.
+#' @param title Report title.
+#' @param subtitle Report subtitle.
+#' @param author Report author.
 #' @param format The output file format for the report, \code{"html"} by default.
 #' Other options are \code{"word"} and \code{"pdf"}.
 #' @param server Logical. If \code{"FALSE"} works with local machines.
 #' Otherwise works in server environments.
 #' @param server_dir_name If \code{"server = TRUE"}, this is the directory name in the server.
 #' @param server_file_name If \code{"server = TRUE"}, this is the file name in the server.
-#' @author Raul Eyzaguirre.
 #' @details It fits a linear model for a CRD and explains the results.
 #'
 #' Under the assumption of fixed effects an ANOVA table is computed. If the ANOVA
@@ -24,9 +23,9 @@
 #' Under the assumption of random effects the model is estimated using REML and the
 #' variance components are shown.
 #' @return It returns an explanation about the CRD fitted model.
+#' @author Raul Eyzaguirre.
 #' @examples
 #' repo.crd(c("trw", "vw"), "geno", pjpz09)
-#'
 #' # With a small data set
 #' temp <- pjpz09[1:18, ]
 #' repo.crd(c("trw", "vw", "crw"), "geno", temp)
@@ -34,7 +33,7 @@
 #' @importFrom utils browseURL
 #' @export
 
-repo.crd <- function(traits, geno, data, maxp = 0.1,
+repo.crd <- function(traits, geno, dfr, maxp = 0.1,
                      title = "Automatic report for a Completely Randomized Design (CRD)",
                      subtitle = NULL,
                      author = "International Potato Center",
@@ -69,7 +68,7 @@ repo.crd <- function(traits, geno, data, maxp = 0.1,
   rmarkdown::render(fileRmd, output_format = format,
                     params = list(traits = traits,
                                   geno = geno,
-                                  data = data,
+                                  dfr = dfr,
                                   maxp = maxp,
                                   title = title,
                                   subtitle = subtitle,
