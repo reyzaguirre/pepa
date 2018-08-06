10af25ff125d8a2e5db4f2c92a92fd130e5c73a4
reyzaguirre <reyzaguirre@gmail.com>
2018-08-06 08:23:46 -0500

    updated

--- R/pty_pesekbaker.R
+++ R/pty_pesekbaker.R
@@ -5,25 +5,26 @@
 #' @param geno The genotypes.
 #' @param env The environments.
 #' @param rep The replications. Must be defined if \code{model = "gxe"}.
-#' @param data The name of the data frame containing the data.
-#' @param means The genotypic means to compute the index, \code{"single"}
-#' or \code{"fitted"}. The default is \code{"single"}. See details for more information.
+#' @param dfr The name of the data frame containing the data.
+#' @param means The genotypic means to compute the index, \code{"single"} or
+#' \code{"fitted"}. The default is \code{"single"}. See details for more information.
 #' @param dgg Desired genetic gains. The default is one standard deviation for each trait.
-#' @param units Units for dgg, \code{"actual"} or \code{"sdu"}. See details for more information.
+#' @param units Units for dgg, \code{"actual"} or \code{"sdu"}.
+#' See details for more information.
 #' @param sf Selected fraction. The default is 0.1.
-#' @param author Author.
+#' @param author Report author.
 #' @param format The output file format for the report, \code{"html"} by default.
 #' Other options are \code{"word"} and \code{"pdf"}.
-#' @author Raul Eyzaguirre.
 #' @details Type \code{?pesekbaker} for additional details.
 #' @return It returns an explanation about the Pesek-Baker index.
+#' @author Raul Eyzaguirre.
 #' @examples
 #' pty.pesekbaker(c("rytha", "bc", "dm", "star", "nocr"), "geno", "loc", "rep", spg)
 #' @import st4gi
 #' @importFrom utils browseURL
 #' @export
 
-pty.pesekbaker <- function(traits, geno, env, rep = NULL, data, means = "single",
+pty.pesekbaker <- function(traits, geno, env, rep = NULL, dfr, means = "single",
                            dgg = NULL, units = "sdu", sf = 0.1,
                            author = "International Potato Center",
                            format = c("html", "word", "pdf")) {
@@ -41,7 +42,7 @@ pty.pesekbaker <- function(traits, geno, env, rep = NULL, data, means = "single"
                                   geno = geno,
                                   env = env,
                                   rep = rep,
-                                  data = data,
+                                  dfr = dfr,
                                   means = means,
                                   dgg = dgg,
                                   units = units,
