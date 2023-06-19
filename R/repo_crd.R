9691eebb452b1ae0830b110e52e0a3a9a254496c
Raul Eyzaguirre <reyzaguirre@gmail.com>
2023-06-19 14:59:05 -0500

    simplify

--- R/repo_crd.R
+++ R/repo_crd.R
@@ -6,15 +6,13 @@
 #' @param trt.lab The labels for treatments.
 #' @param eu The experimental unit. Must be defined in case of subsamples.
 #' @param dfr The name of the data frame.
+#' @param mc Logical. If \code{"mc = TRUE"} multiple comparison tests are included
+#' even if the factor effect is not significat. Default to \code{"mc = FALSE"}.
 #' @param title Report title.
 #' @param subtitle Report subtitle.
 #' @param author Report author.
 #' @param format The output file format for the report, \code{"html"} by default.
 #' Other options are \code{"word"} and \code{"pdf"}.
-#' @param server Logical. If \code{"FALSE"} works with local machines.
-#' Otherwise works in server environments.
-#' @param server_dir_name If \code{"server = TRUE"}, this is the directory name in the server.
-#' @param server_file_name If \code{"server = TRUE"}, this is the file name in the server.
 #' @details It fits a linear model for a CRD and explains the results.
 #'
 #' Under the assumption of fixed effects an ANOVA table is computed.
@@ -36,37 +34,19 @@
 #' @importFrom utils browseURL
 #' @export
 
-repo.crd <- function(traits, trt, trt.lab = "treatment", eu = NULL, dfr,
+repo.crd <- function(traits, trt, trt.lab = "treatment", eu = NULL, dfr, mc = FALSE,
                      title = "Automatic report for a Completely Randomized Design (CRD)",
                      subtitle = NULL,
                      author = "International Potato Center",
-                     format = c("html", "word", "pdf"),
-                     server = FALSE,
-                     server_dir_name = "directory",
-                     server_file_name = "filename") {
+                     format = c("html", "word", "pdf")) {
 
   format <- paste0(match.arg(format), "_document")
   dirfiles <- system.file(package = "pepa")
 
-  if (!server) {
-
-    fileRmd <- paste0(dirfiles, "/rmd/crd.Rmd")
-    fileURL <- paste0(dirfiles, "/rmd/crd.html")
-    fileDOCX <- paste0(dirfiles, "/rmd/crd.docx")
-    filePDF <- paste0(dirfiles, "/rmd/crd.pdf")
-
-  } else {
-
-    dirfiles <- server_dir_name
-
-    # Only Markdown and Word files
-
-    fileRmd <- paste0(dirfiles, "crd.Rmd")
-    fileRmd_server_name <- paste0(dirfiles, server_file_name, ".Rmd")
-    fileDOCX <- paste0(dirfiles, "crd.docx")
-    fileDOCX_server_name <- paste0(dirfiles, server_file_name, ".docx")
-
-  }
+  fileRmd <- paste0(dirfiles, "/rmd/crd.Rmd")
+  fileURL <- paste0(dirfiles, "/rmd/crd.html")
+  fileDOCX <- paste0(dirfiles, "/rmd/crd.docx")
+  filePDF <- paste0(dirfiles, "/rmd/crd.pdf")
 
   rmarkdown::render(fileRmd, output_format = format,
                     params = list(traits = traits,
@@ -74,25 +54,18 @@ repo.crd <- function(traits, trt, trt.lab = "treatment", eu = NULL, dfr,
                                   trt.lab = trt.lab,
                                   eu = eu,
                                   dfr = dfr,
+                                  mc = mc,
                                   title = title,
                                   subtitle = subtitle,
                                   author = author))
 
-  if (!server) {
+  if (format == "html_document")
+    try(browseURL(fileURL))
 
-    if (format == "html_document")
-      try(browseURL(fileURL))
+  if (format == "word_document")
+    try(system(paste("open", fileDOCX)))
 
-    if (format == "word_document")
-      try(system(paste("open", fileDOCX)))
-
-    if (format == "pdf_document")
-      try(system(paste("open", filePDF)))
-
-  } else {
-
-    file.copy(fileDOCX, fileDOCX_server_name, overwrite = TRUE)
-
-  }
+  if (format == "pdf_document")
+    try(system(paste("open", filePDF)))
 
 }
