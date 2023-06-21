c14b99b4aed4096f1d834bf35320f45178ee1a14
Raul Eyzaguirre <reyzaguirre@gmail.com>
2023-06-21 14:25:18 -0500

    pe parameter in split plot multiple comparisons in split plot NA comment included

--- R/repo_spld.R
+++ R/repo_spld.R
@@ -8,15 +8,14 @@
 #' @param sspf The sub sub plot factor (if any).
 #' @param rep The replications.
 #' @param dfr The name of the data frame.
+#' @param pe Logical. If \code{"pe = TRUE"} multiple comparison tests for principal effects
+#' are included even if interaction is significat, only in the case of 2 factors.
+#' Default to \code{"pe = FALSE"}.
 #' @param title Report title.
 #' @param subtitle Report subtitle.
 #' @param author Report author.
 #' @param format The output file format for the report, \code{"html"} by default.
 #' Other options are \code{"word"} and \code{"pdf"}.
-#' @param server Logical. If \code{"FALSE"} works with local machines.
-#' Otherwise works in server environments.
-#' @param server_dir_name If \code{"server = TRUE"}, this is the directory name in the server.
-#' @param server_file_name If \code{"server = TRUE"}, this is the file name in the server.
 #' @details It fits a linear model for a split-plot or a split-split-plot design and explains
 #' the results.
 #' @return It returns an explanation about the split-plot or a split-split-plot design
@@ -39,14 +38,11 @@
 #' @importFrom utils browseURL
 #' @export
 
-repo.spld <- function(traits, mpf, spf, sspf = NULL, rep, dfr,
+repo.spld <- function(traits, mpf, spf, sspf = NULL, rep, dfr, pe = FALSE,
                       title = NULL,
                       subtitle = NULL,
                       author = "International Potato Center",
-                      format = c("html", "word", "pdf"),
-                      server = FALSE,
-                      server_dir_name = "directory",
-                      server_file_name = "filename") {
+                      format = c("html", "word", "pdf")) {
 
   format <- paste0(match.arg(format), "_document")
   dirfiles <- system.file(package = "pepa")
@@ -63,25 +59,10 @@ repo.spld <- function(traits, mpf, spf, sspf = NULL, rep, dfr,
       title <- "Automatic report for a split-split-plot design"
   }
 
-  if (!server) {
-
-    fileRmd <- paste0(dirfiles, "/rmd/", fn, ".Rmd")
-    fileURL <- paste0(dirfiles, "/rmd/", fn, ".html")
-    fileDOCX <- paste0(dirfiles, "/rmd/", fn, ".docx")
-    filePDF <- paste0(dirfiles, "/rmd/", fn, ".pdf")
-
-  } else {
-
-    dirfiles <- server_dir_name
-
-    # Only Markdown and Word files
-
-    fileRmd <- paste0(dirfiles, fn, ".Rmd")
-    fileRmd_server_name <- paste0(dirfiles, server_file_name, ".Rmd")
-    fileDOCX <- paste0(dirfiles, fn, ".docx")
-    fileDOCX_server_name <- paste0(dirfiles, server_file_name, ".docx")
-
-  }
+  fileRmd <- paste0(dirfiles, "/rmd/", fn, ".Rmd")
+  fileURL <- paste0(dirfiles, "/rmd/", fn, ".html")
+  fileDOCX <- paste0(dirfiles, "/rmd/", fn, ".docx")
+  filePDF <- paste0(dirfiles, "/rmd/", fn, ".pdf")
 
   rmarkdown::render(fileRmd, output_format = format,
                     params = list(traits = traits,
@@ -90,25 +71,18 @@ repo.spld <- function(traits, mpf, spf, sspf = NULL, rep, dfr,
                                   sspf = sspf,
                                   rep = rep,
                                   dfr = dfr,
+                                  pe = pe,
                                   title = title,
                                   subtitle = subtitle,
                                   author = author))
 
-  if (!server) {
+  if (format == "html_document")
+    try(browseURL(fileURL))
 
-    if (format == "html_document")
-      try(browseURL(fileURL))
+  if (format == "word_document")
+    try(system(paste("open", fileDOCX)))
 
-    if (format == "word_document")
-      try(system(paste("open", fileDOCX)))
-
-    if (format == "pdf_document")
-      try(system(paste("open", filePDF)))
-
-  } else {
-
-    file.copy(fileDOCX, fileDOCX_server_name, overwrite = TRUE)
-
-  }
+  if (format == "pdf_document")
+    try(system(paste("open", filePDF)))
 
 }
